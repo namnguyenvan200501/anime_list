053a6bbff88dd71a882a02e9afb1a6f7a30cfa7b
NguyenVanNam <nvn.200501@gmail.com>
2024-04-04 21:31:51 +0700

    Initial Commit

---
+++ AnimeList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeList
+//
+//  Created by Nam Nguyễn on 04/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
